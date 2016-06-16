14788f061624276740c8af00567c343c3b98056a
avue7 <avue7@mail.csuchico.edu>
2016-06-16 02:15:19 -0700

    ALRITE...got the output to work except for checking to see if its a word ladder or not. Output for the steps that needs to show up or not are fixed.

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -1,16 +1,44 @@
 class HomeController < ApplicationController
   def index
-    load "#{Rails.root}/lib/ladder.rb"
+    # Load the file in the library to use its helper functions.
+    load "#{Rails.root}/lib/ladder.rb" 
+    @start_word = dictionary.shuffle.first
+    @end_word = dictionary.shuffle.last 
+    while @start_word == @end_word do #making sure the start and end words dont match
       @start_word = dictionary.shuffle.first
       @end_word = dictionary.shuffle.last
+    end  
   end
 
   def show
+    # Load the file in the library for use of helper functions
     load "#{Rails.root}/lib/ladder.rb"
-    @step_1 = params[:step1]
+    @status_winner == false
+    @steps = []
+    # Set the symbols to the member variables so we can use them outside
+    # of this function and class.
+    @start_word = params[:start_word]
+    @step_1 = params[:step1] 
     @step_2 = params[:step2]
     @step_3 = params[:step3]
     @step_4 = params[:step4]
     @step_5 = params[:step5]
-  end
+    @end_word = params[:end_word]
+    # Test to see if steps are empty, if not push onto array steps
+    if params[:step1].present?
+      @steps.push(@step_1)
+    end
+    if params[:step2].present?
+      @steps.push(@step_2)
+    end
+    if params[:step3].present?
+      @steps.push(@step_3)
+    end
+    if params[:step4].present?
+      @steps.push(@step_4)
+    end
+    if params[:step5].present?
+      @steps.push(@step_5)
+    end
+ end
 end
