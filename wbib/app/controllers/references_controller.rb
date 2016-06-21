fae1fa61eefcf22ec337de99c5a05d9fc082562a
avue7 <avue7@mail.csuchico.edu>
2016-06-21 16:57:21 -0700

    Got the show button to work. And format the pages to the way I like it. Need to do the edit, delete, and new reference button

--- wbib/app/controllers/references_controller.rb
+++ wbib/app/controllers/references_controller.rb
@@ -7,10 +7,20 @@ class ReferencesController < ApplicationController
   end
 
   # GET /references/1
-  def show
-
+  def show 
+   @reference = Reference.find(params[:id])
   end
 
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_reference
+      @reference = Reference.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def reference_params
+      params.require(:reference).permit(:URL, :topic, :annotation,)
+    end
   # GET /references/new
 #  def new 
  #   @reference = Reference.new
--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,2 +1,5 @@
 class Reference < ActiveRecord::Base
+  validates :URL, presence: true
+  validates :topic, presence: true
+  validates :annotation, presence: true
 end
