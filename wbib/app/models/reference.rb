0dfcf825d48a14f0dfc96ad68f79f580b3ae0612
avue7 <avue7@mail.csuchico.edu>
2016-06-21 23:12:46 -0700

    Fix the URL to work for adding and editing.

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,6 +1,5 @@
 class Reference < ActiveRecord::Base
   before_validation :my_function
-  validates :click_through_url, URL:true
   validates :URL, presence: true
   validates :topic, presence: true
   validates :annotation, presence: true
