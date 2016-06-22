3c08f376e74fde6bd164791bac013d3db1e61000
avue7 <avue7@mail.csuchico.edu>
2016-06-21 20:00:44 -0700

    The New Reference works now but the format needs to be fixed. Also somehow the time invertedly changed on its own upon adding a new reference. I will next fix the delete function and the format for the view on adding a new reference. Then I will come back and figure out how to add an object to the server without it changing the created_at time at server time.

--- wbib/app/controllers/references_controller.rb
+++ wbib/app/controllers/references_controller.rb
@@ -3,47 +3,50 @@ class ReferencesController < ApplicationController
 
   #GET /references
   def index
-   @references = Reference.all
+    @references = Reference.all
   end
 
   # GET /references/1
   def show 
-   @reference = Reference.find(params[:id])
+    @reference = Reference.find(params[:id])
   end
   
   # GET /references/1/edit
   def edit
-   @reference = Reference.find(params[:id])
+    @reference = Reference.find(params[:id])
   end
   
   # PATCH/PUT references/1
   def update
-   @reference = Reference.find(params[:id])
-   if @reference.update(reference_params)
-     redirect_to @reference, notice: 'Reference was successfully updated.'
-   else
-     render :edit
-   end
+    @reference = Reference.find(params[:id])
+    if @reference.update(reference_params)
+      redirect_to @reference, notice: 'Reference was successfully updated.'
+    else
+      render :edit
+    end
   end
-
-
-
-
+  
+  # GET /references/new
+  def new 
+    @reference = Reference.new
+  end
+  
+  def create
+    @reference = Reference.new(params.require(:reference).permit(:URL, :topic, :annotation))
+    if @reference.save
+      redirect_to @reference, notice: 'New reference was successfully created!'
+    else
+      render :new
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_reference
-      @reference = Reference.find(params[:id])
-    end
-
+  def set_reference
+    @reference = Reference.find(params[:id])
+   end
     # Never trust parameters from the scary internet, only allow the white list through.
-    def reference_params
-      params.require(:reference).permit(:URL, :topic, :annotation,)
-    end
-  # GET /references/new
-#  def new 
- #   @reference = Reference.new
- # end
- 
-  # GET /
+  def reference_params
+    params.require(:reference).permit(:URL, :topic, :annotation)
+  end
 end
