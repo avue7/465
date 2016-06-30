e12f37a64a8ab1ab58e817221ce487ad9665e170
avue7 <avue7@mail.csuchico.edu>
2016-06-29 19:18:17 -0700

    fixed the sign in to spec. Need to work on uploading next

--- img/app/models/user.rb
+++ img/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # Validate to make sure name field for signing up user is not blank
+  validates :name, presence: true
 end
